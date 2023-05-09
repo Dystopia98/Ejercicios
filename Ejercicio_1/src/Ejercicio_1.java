927eaac9a6f5d7d7db5f2a0da2524c7d4c905205
Nicolás Fernández <nicolasf22@live.com.ar>
2023-05-09 17:00:37 -0300

    Ejercicio 1

---
+++ Ejercicio_1/src/Ejercicio_1.java
@@ -0,0 +1,15 @@
+/*
+Crear un proyecto de Java y definir al menos 6 variables en tu IDE de distintos tipos de datos.
+ */
+public class Ejercicio_1 {
+
+    public static void main(String[] args) {
+        int uno ;
+        boolean dos;
+        String tres;
+        float cuatro;
+        long cinco;
+        double seis;
+    }
+    
+}
