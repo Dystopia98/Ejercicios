78dfb0bc652e37be938e35162e7c0eab96114bae
Nicolás Fernández <nicolasf22@live.com.ar>
2023-05-09 17:05:28 -0300

    Ejercicio 3

---
+++ Ejercicio_3/src/Ejercicio_3.java
@@ -0,0 +1,41 @@
+/*
+Define variables donde puedas alojar los resultados y prueba usar los operadores de cada tipo.
+ */
+public class Ejercicio_3 {
+    public static void main(String[] args) {
+        //Operadores aritmeticos
+        int suma;
+        int resta;
+        int multiplicacion;
+        int division;
+        int modulo;
+        suma = 1+1;
+        resta = 1-1;
+        multiplicacion = 1*1;
+        division = 1/1;
+        modulo = 1%1;
+        //Operadores unarios
+        int mas;
+        int menos;
+        int incremento;
+        int decremento;
+        mas = +1;
+        menos = -1;
+        // incremento = ++1 ;
+        // decremento = --1 ;
+        //Operadores de igualdad y relación
+        boolean igual;
+        boolean distinto;
+        boolean mayorque;
+        boolean mayoroigual;
+        boolean menorque;
+        boolean menoroigual;
+        igual = 1 == 1;
+        distinto = 1!= 2;
+        mayorque = 1 > 0;
+        mayoroigual = 1 >= 1;
+        menorque = 1 < 2;
+        menoroigual = 1 <= 1;
+    }
+    
+}
