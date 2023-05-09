90a3065b63d61983cc77713078c24dfd7a121c5f
Nicolás Fernández <nicolasf22@live.com.ar>
2023-05-09 17:16:27 -0300

    Ejercicio 4

---
+++ Ejercicio_4/src/Ejercicio_4.java
@@ -0,0 +1,16 @@
+/*
+Define una variable que aloje tu nombre y otra que guarde tu edad.
+Imprime ambas variables por pantalla.
+Recomendamos que hagan el siguiente experimento: tipear en minúsculas la palabra sout y
+apenas terminamos de escribirla tocar el botón tab o mejor dicho tabular.
+Esto nos va a generar un System.out.println() para poder escribir lo que queramos.
+ */
+public class Ejercicio_4 {
+    public static void main(String[] args) {
+        String nombre = "Nicolás";
+        int edad = 24;
+        System.out.println("Nombre :"+nombre);
+        System.out.println("Edad :"+edad+"años");
+    }
+    
+}
