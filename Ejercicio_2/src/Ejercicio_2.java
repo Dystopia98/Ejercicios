d3f98b8f1ac4d2f5a650e37ec6937577bb9b9091
Nicolás Fernández <nicolasf22@live.com.ar>
2023-05-09 17:02:47 -0300

    Ejercicio 2

---
+++ Ejercicio_2/src/Ejercicio_2.java
@@ -0,0 +1,15 @@
+/*
+¿Recuerdas las variables que creaste en el ejercicio anterior? Ahora deberás asignarles un valor.
+ */
+public class Ejercicio_2 {
+
+    public static void main(String[] args) {
+        int uno = 1;
+        boolean dos = true;
+        String tres = "tres";
+        float cuatro = 10;
+        long cinco = 10000;
+        double seis = 0.20;
+    }
+
+}
