4392e41c749452b8b666b4780100f21eb603fd72
Nicolás Fernández <nicolasf22@live.com.ar>
2023-05-09 18:05:28 -0300

    Ejercicio 13

---
+++ Ejercicio_13/src/Ejercicio_13.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Ejercicio_13 {
+/*
+ Crea un vector llamado ‘Equipo’ cuya dimensión sea la cantidad de compañeros de
+equipo y define su tipo de dato de tal manera que te permita alojar sus nombres más adelante.
+*/
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        String[] Equipo;
+        Equipo = new String[9];
+    }
+
+}
